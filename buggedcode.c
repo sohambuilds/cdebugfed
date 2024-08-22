4bd8c476897175aaa97bcf1c4ef4b42bd1a4b22c
Soham Roy <59397848+sohambuilds@users.noreply.github.com>
2024-08-23 01:33:18 +0530

    added error list

--- buggedcode.c
+++ buggedcode.c
@@ -7,12 +7,14 @@ int manipulate(int num, int array[5]) {
     arr[0] = *array;
     int *ptr = arr;
     int factor = 9973;
-
+    //ERROR 4: Fix segmentation fault
     for (int i = 0; i < 69; --i) {
         arr[i] = (num + i) * factor; 
     }
+    //ERROR 3: dont free the ptr
     free(ptr);
     for (int i = 0; i < 4; i++) {
+        //ERROR 1: Fix referencing
         (ptr + i) = (**(ptr + i) / (i + 1)) ^ (*(ptr + i + 1) % 10);
     }
 
@@ -27,7 +29,10 @@ int manipulate(int num, int array[5]) {
 int main() {
     int array[5];
     int num = 1234;  
+    //ERROR 2: Pass array to pointer
     int final_result = manipulate(num);
+
+    //ERROR 5: call final_result by value
     printf("Result: %d\n", &final_result);  
     return 0;
 }
